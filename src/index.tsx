9296b8392413f81585c41bd1d19afb9b8397d298
ireneasensio <irene.asensio@dentsu.com>
2023-05-10 19:56:44 +0200

    initial commit of new project

--- src/index.tsx
+++ src/index.tsx
@@ -1,18 +1,16 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
-import { BrowserRouter as Router } from "react-router-dom";
-
-
-ReactDOM.render(
-  <Router>
+const root = ReactDOM.createRoot(
+  document.getElementById('root') as HTMLElement
+);
+root.render(
+  <React.StrictMode>
     <App />
-  </Router>,
-
-  document.getElementById('root')
+  </React.StrictMode>
 );
 
 // If you want to start measuring performance in your app, pass a function
